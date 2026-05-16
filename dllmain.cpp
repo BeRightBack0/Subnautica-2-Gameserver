a917dd32d2dbb855bb869b710498af3e35af309e
BeRightBack0 <131478455+BeRightBack0@users.noreply.github.com>
2026-05-16 10:10:22 +0200

    fix the ass gisclient crashing randomly maybe ingame today lol

--- dllmain.cpp
+++ dllmain.cpp
@@ -2,17 +2,121 @@
 #include "pch.h"
 
 
+int ReturnOne() {
+    return 1;
+}
+void returan() {
+    return;
+}
+
+
+
+enum ENetMode  
+{
+    NM_Standalone,  
+    NM_DedicatedServer,  
+    NM_ListenServer,
+    NM_Client,  
+    NM_MAX,  
+};
+
+void CopyToClipboard(const std::string& text)
+{
+    if (!OpenClipboard(nullptr)) return;
+    EmptyClipboard();
+
+    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, text.size() + 1);
+    if (!hMem) { CloseClipboard(); return; }
+
+    memcpy(GlobalLock(hMem), text.c_str(), text.size() + 1);
+    GlobalUnlock(hMem);
+
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
+}
+
+namespace EBrowseReturnVal
+{
+    enum Type
+    {
+        /** Successfully browsed to a new map. */
+        Success,
+        /** Immediately failed to browse. */
+        Failure,
+        /** A connection is pending. */
+        Pending,
+    };
+}
+
+DefineOriginal(EBrowseReturnVal::Type, sub_479DE10, UEngine* Engine, FWorldContext& WorldContext, FURL URL, FString& Error);
+// uengine::browse called like a milion times on the client
+EBrowseReturnVal::Type __fastcall sub_479DE10(UEngine* Engine, FWorldContext& WorldContext, FURL URL, FString& Error) {
+
+    URL.Map = FString(L"/Game/Maps/Main/L_Main");
+    URL.Protocol = FString(L"unreal");
+  //  URL.Host = FString(L"127.0.0.1");
+    URL.Port = 7777;
+
+   return sub_479DE10OG(Engine, WorldContext, URL, Error);
+}
+
+bool jew() {
+    std::cout << "called yo" << std::endl;
+    return false;
+}
+DefineOriginal(char, jews, UEngine* Engine, FWorldContext* WorldContext, FURL* URL, UPendingNetGame* Pending, FString* Error);
+char jews(UEngine* Engine, FWorldContext* WorldContext, FURL* URL, UPendingNetGame* Pending, FString* Error) {
+    std::cout << "hello" << std::endl;
+
+    URL->Map = FString(L"/Game/Maps/Main/L_Main");
+    //URL->Protocol = FString(L"unreal");
+   // URL->Host = FString(L"127.0.0.1");
+    //URL->Port = 7777;
+
+    return jewsOG(Engine, WorldContext, URL, Pending, Error);
+}
+
+
 void Main() {
     AllocConsole();
+    MH_Initialize();
     FILE* File = nullptr;
     freopen_s(&File, "CONOUT$", "w+", stdout);
     freopen_s(&File, "CONOUT$", "w+", stderr);
 
+    *(bool*)(ImageBase + 0xCC24A42) = false; // gisclient
+    *(bool*)(ImageBase + 0xCC24A43) = true; // gisserver
+
+    *(int*)(ImageBase + 0xCE78798) = 7; // random ass log for listening
+    *(int*)(ImageBase + 0xCF118F8) = 1; // ass log that is spammed when u inject late 
+
+
+    //UEngine* Engine = SDK::UEngine::GetEngine();
+    //UObject* NewObject = UGameplayStatics::SpawnObject(Engine->ConsoleClass, Engine->GameViewport);
+    //Engine->GameViewport->ViewportConsole = static_cast<UConsole*>(NewObject);
+    UKismetSystemLibrary::ExecuteConsoleCommand(GetWorld(), L"net.AllowEncryption 0", nullptr); // we use it on fortnite so idk we will use it here
+    Hook(ImageBase + 0x47088C0, ReturnOne, nullptr); // GetNetMode is not inlined
+
+  //  auto Gamemode = (AUWELobbyGameMode*)UGameplayStatics::GetGameMode(GetWorld());
+    //Gamemode->StartNewServerGame(ASN2WorldGameMode::StaticClass());
+
+   // UUWEGameModeTypeStatics
+
+  //  Hook(ImageBase + 0x47AFEE0, jews, (void**)&jewsOG); // uengine::loadmap
+    //Hook(ImageBase + 0x479DE10, sub_479DE10, (void**)&sub_479DE10OG);
+   // Hook(ImageBase + 0x5AEB3E0, returan, nullptr);
+    //VFTHook<AGameMode>(0x950 / 8, jew, nullptr); // ready to start match 
+
+  //  VFTHookEvery<UGameInstance>(0x5F, ReturnOne);
+
+
+    MH_EnableHook(MH_ALL_HOOKS);
+  //  UGameEngine::GetEngine()->GameInstance->LocalPlayers.Remove(0);
+   // UKismetSystemLibrary::ExecuteConsoleCommand(GetWorld(), L"open /Game/Maps/Main/L_Main", nullptr);
+    // switch or open level might be better ngl
 
 
 
-    UGameEngine::GetEngine()->GameInstance->LocalPlayers.Remove(0);
-    UKismetSystemLibrary::ExecuteConsoleCommand(GetWorld(), L"open /Game/Maps/Main/L_Main", nullptr);
 }
 BOOL APIENTRY DllMain(HMODULE hModule,
     DWORD  ul_reason_for_call,
--- pch.h
+++ pch.h
@@ -8,7 +8,6 @@
 #define PCH_H
 
 // add headers that you want to pre-compile here
-#pragma warning(disable: 4606) // MSVC
 #include "framework.h"
 #include <windows.h>
 #include <iostream>
@@ -25,12 +24,13 @@
 #include <sstream>
 #include "SDK/SDK/Basic.hpp"
 #include "SDK/SDK/CoreUObject_structs.hpp"
+#include "SDK/SDK/Subnautica2_classes.hpp"
 #include "SDK/SDK/CoreUObject_classes.hpp"
 #include "SDK/SDK/Engine_structs.hpp"
 #include "SDK/SDK/Engine_classes.hpp"
 #include "SDK/SDK/GameplayAbilities_structs.hpp"
 #include "SDK/SDK/GameplayAbilities_classes.hpp"
-
+#include "MinHook.h"
 #include <set>
 #include <atomic>
 
@@ -44,4 +44,56 @@ inline UWorld* GetWorld()
     return UWorld::GetWorld();
 }
 
+inline MH_STATUS Hook(uintptr_t Address, PVOID Hook, void** Original) {
+	if (Address - ImageBase == 0x0) return MH_ERROR_MEMORY_ALLOC;
+
+	return MH_CreateHook((LPVOID)Address, Hook, Original);
+}
+
+#define DefineOriginal(_Rt, _Name, ...) static inline _Rt (*_Name##OG)(##__VA_ARGS__); static _Rt _Name(##__VA_ARGS__);
+
+
+template<typename T>
+inline void VFTHook(int index, void* detour, void** original = nullptr)
+{
+	void* object = T::GetDefaultObj();
+	if (!object) return;
+
+	void** vtable = *(void***)object;
+	if (!vtable || !vtable[index])
+		return;
+
+	if (original)
+		*original = vtable[index];
+
+	DWORD oldProtect;
+	if (VirtualProtect(&vtable[index], sizeof(void*), PAGE_EXECUTE_READWRITE, &oldProtect))
+	{
+		vtable[index] = detour;
+		VirtualProtect(&vtable[index], sizeof(void*), oldProtect, &oldProtect);
+	}
+}
+
+template<typename T>
+inline void VFTHookEvery(int index, void* detour)
+{
+	for (int i = 0; i < UObject::GObjects->Num(); i++) {
+		UObject* Object = UObject::GObjects->GetByIndex(i);
+
+		if (!Object) continue;
+
+		if (Object->IsA(T::StaticClass())) {
+			void** vtable = *(void***)Object;
+			if (!vtable || !vtable[index])
+				continue;
+
+			DWORD oldProtect;
+			if (VirtualProtect(&vtable[index], sizeof(void*), PAGE_EXECUTE_READWRITE, &oldProtect))
+			{
+				vtable[index] = detour;
+				VirtualProtect(&vtable[index], sizeof(void*), oldProtect, &oldProtect);
+			}
+		}
+	}
+}
 #endif //PCH_H
